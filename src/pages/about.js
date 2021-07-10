2cab35bdb9afa28d62381183160d921523bf9ac3
Neelaveni2021 <neelaveni.dsvb@gmail.com>
2021-07-11 02:00:37 +0530

    color

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,7 @@ import Layout from '../Components/layout'
 const Aboutpage = () => {
     return (
         <Layout pageTitle='About' pageHeading='About Page' children>
-           Welcome to  About page
+          <h1> Welcome to  About page</h1>
       </Layout>
     )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-import {Link} from 'gatsby'
+
 import Layout from '../Components/layout'
 
 const contact = () => {
     return(
         <Layout pageTitle='Contact Us' pageHeading='Contact' children>
-           Welcome to my page
+           <h1>Feel free to dump your doubts by contacting us </h1>
         </Layout>
 
     )

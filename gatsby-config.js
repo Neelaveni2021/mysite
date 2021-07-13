91d99135305345b1c66cd3c885d0edfac3234fe0
Neelaveni2021 <neelaveni.dsvb@gmail.com>
2021-07-13 15:04:18 +0530

    added picture

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,11 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "mysite",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud", "gatsby-plugin-image"]
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+     "gatsby-plugin-sharp",
+      
+     "gatsby-transformer-sharp",  
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,36 @@
+import { StaticImage } from "gatsby-plugin-image";
 import React from 'react';
 
-import Layout from '../Components/layout'
-import { StaticImage } from 'gatsby-plugin-image';
+import Layout from '../Components/layout';
+
 
 const Homepage = () => {
   return (
 
     <Layout pageTitle='Home' pageHeading='Home Page' children>
          <h1>welcome to home page</h1>  
+
+         <StaticImage 
+         src="https://www.parkinson.org/sites/default/files/PD-diet.png"
+         alt="food food"
+         />
+         
+        
          
          
       </Layout>
-      
   )
+}
+
+ 
+  
+  
     
     
   
    
 
-  }
+  
 
 export default Homepage
+
